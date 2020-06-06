db60280187969d436e6808ee0b098e5c5b3f9475
Igor Cedro <igor.cedro@anexxa.com.br>
2020-06-06 16:51:07 -0300

    adicionando imagens

--- src/controllers/itemsController.ts
+++ src/controllers/itemsController.ts
@@ -10,7 +10,8 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: process.env.APP_URL + "/uploads/" + item.image,
+                image_url:
+                    process.env.APP_URL + "/uploads/system-files/" + item.image,
             };
         });
 
